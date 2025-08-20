2e36ccb31e9987c9af793c2317836f6f3ab771b9
gajju44 <gajjunaphade@gmail.com>
2025-08-20 13:21:13 +0530

    added dashboard route

---
+++ client/app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import DatasetCard from '@/components/SharedComponents/DatasetCompo/DatasetCard'
+function page() {
+  return (
+    <>
+    
+      <h1>Dashboard</h1>
+      <DatasetCard/>
+    </>
+  )
+}
+
+export default page
