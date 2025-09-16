49e5ce375da84e8bfc9db03f0675e648638438f9
ankitkatole <katoleankit06@gmail.com>
2025-09-17 01:01:52 +0530

    Update:  BuyerDatasetDeliveryScehma

--- server/src/models/BuyerDatasetDelivery.js
+++ server/src/models/BuyerDatasetDelivery.js
@@ -1,11 +1,28 @@
 const mongoose = require('mongoose');
 
 const buyerDatasetDeliverySchema = new mongoose.Schema({
-  datasetId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'DatasetCatalogue', index: true },
-  buyerAddress: { type: String, required: true, index: true },
-  buyerId: { type: String },
-  watermarkedEncryptedDataCID: { type: String, required: true },
-  encryptedSymmetricKey: { type: String, required: true },
+  datasetId: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    required: true,
+    ref: 'DatasetCatalogue', 
+    index: true
+  },
+  buyerAddress: {
+    type: String,
+    required: true,
+    index: true
+  },
+  buyerId: { 
+    type: String 
+  },
+  watermarkedEncryptedDataCID: { 
+    type: String, 
+    required: true
+ },
+  encryptedSymmetricKey: {
+    type: String,
+    required: true
+  },
 }, { timestamps: true });
 
 buyerDatasetDeliverySchema.index({ datasetId: 1, buyerAddress: 1 }, { unique: true });
