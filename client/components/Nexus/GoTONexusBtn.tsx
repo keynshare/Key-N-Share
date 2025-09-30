222451685ba8b022b49f744f0af63b10e8880e47
gajju44 <gajjunaphade@gmail.com>
2025-09-30 22:28:32 +0530

    added better error handling in delivery page and

--- client/components/Nexus/GoTONexusBtn.tsx
+++ client/components/Nexus/GoTONexusBtn.tsx
@@ -8,10 +8,8 @@ function GoTONexusBtn() {
     const pathname = usePathname();
   return (
     <>
-  <Link href='/nexus' title='Chat with Nexus' className={`bg-black z-50 hover:text-white transition-all duration-500 dark:hover:text-white fixed bottom-10 right-7 dark:bg-gray-100 text-white dark:text-black px-4 py-4 rounded-full hover:bg-[#ff7b00] dark:hover:bg-[#ff7b00] ` + (pathname === '/nexus' ? 'hidden' : '')}>
+  <Link href='/nexus' title='Chat with Nexus' className={`bg-black z-50 hover:text-white transition-all duration-500 dark:hover:text-white fixed bottom-24 lg:bottom-10 right-7 dark:bg-gray-100 text-white dark:text-black px-4 py-4 rounded-full hover:bg-[#ff7b00] dark:hover:bg-[#ff7b00] ` + (pathname === '/nexus' ? 'hidden' : '')}>
     <Bot  />
-    
-
   </Link>
   </>
   )
